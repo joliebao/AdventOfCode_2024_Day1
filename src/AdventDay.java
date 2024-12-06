067fbc506edd7269c234987e894afaf355e2eafa
Testing <s@gmail.com>
2024-12-06 11:05:38 -0500

    list comparison editing

--- src/AdventDay.java
+++ src/AdventDay.java
@@ -12,31 +12,26 @@ public class AdventDay {
         totalSum = 0;
     }
 
-//    public void extraRemover(){
-//        list1 = list1.replace(",","");
-//        list1 = list1.replace("[", "");
-//        list1 = list1.replace("]","");
-//        list2 = list2.replace(",","");
-//        list2 = list2.replace("[", "");
-//        list2 = list2.replace("]","");
-//    }
-
     public int compareLists(){
         int min1 = 100000000;
         int min2 = 100000000;
-        for (int i = 0; i < list1.size(); i++){
-            if (Integer.parseInt(list1.get(i)) < min1){
-                min1 = Integer.parseInt(list1.get(i));
+        int counter = 0;
+        while (counter < list1.size()){
+            for (int i = 0; i < list1.size(); i++) {
+                System.out.println(min1);
+                if (Integer.parseInt(list1.get(i)) < min1) {
+                    min1 = Integer.parseInt(list1.get(i));
+                }
             }
+            for (int i2 = 0; i2 < list2.size(); i2++){
+                if (Integer.parseInt(list2.get(i2)) < min2) {
+                    min2 = Integer.parseInt(list2.get(i2));
+                }
+            }
+            totalSum += Math.abs(min1 - min2);
+            counter++;
         }
 
-        for (int i = 0; i < list2.size(); i++){
-            if (Integer.parseInt(list2.get(i)) < min2){
-                min2 = Integer.parseInt(list2.get(i));
-            }
-        }
-        totalSum += Math.abs(min1 - min2);
-
-        return Math.abs(min1 - min2);
+        return totalSum;
     }
 }
