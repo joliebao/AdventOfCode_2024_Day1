524a58d10350a4b3cab5ee5fed41f199bbd9ec3b
joliebao <baojolie@gmail.com>
2024-12-09 00:38:22 -0500

    Part 1 - minimum is not changing

--- src/AdventDay.java
+++ src/AdventDay.java
@@ -5,31 +5,38 @@ public class AdventDay {
     private List<String> list1 = new ArrayList<String>();
     private List<String> list2 = new ArrayList<String>();
     private int totalSum;
+    private int min1;
+    private int min2;
 
     public AdventDay(ArrayList<String> list1, ArrayList<String> list2){
         this.list1 = list1;
         this.list2 = list2;
         totalSum = 0;
+        min1 = 100000000;
+        min2 = 100000000;
     }
 
     public int compareLists(){
-        int min1 = 100000000;
-        int min2 = 100000000;
         int counter = 0;
         while (counter < list1.size()){
-            for (int i = 0; i < list1.size(); i++) {
-                System.out.println(min1);
+            for (int i = 0; i < list1.size(); i++){
                 if (Integer.parseInt(list1.get(i)) < min1) {
                     min1 = Integer.parseInt(list1.get(i));
+                    list1.add(i, "0000000");
                 }
             }
+            System.out.println("MIN1: " + min1);
             for (int i2 = 0; i2 < list2.size(); i2++){
                 if (Integer.parseInt(list2.get(i2)) < min2) {
                     min2 = Integer.parseInt(list2.get(i2));
                 }
             }
-            totalSum += Math.abs(min1 - min2);
+//            System.out.println("MIN2: " + min2);
+            totalSum += (Math.abs(min1 - min2));
+//            System.out.println("TOTAL:" + totalSum);
             counter++;
+            min1 = 100000000;
+            min2 = 100000000;
         }
 
         return totalSum;
--- src/AdventSolver.java
+++ src/AdventSolver.java
@@ -18,17 +18,17 @@ public class AdventSolver {
             File f = new File(fileName);
             Scanner s = new Scanner(f);
             while (s.hasNextLine()) {
-                String line = (s.next()).substring(0,1);
-                String line2 = (s.next()).substring(0,1);
+                String line = (s.next()).substring(0, 1);
+                String line2 = (s.next()).substring(0, 1);
                 if (!line.equals("")) {
                     fileData.add(line);
                     fileData2.add(line2);
                 }
-                // add code so that other line gets separated by the space in between
-            AdventDay p = new AdventDay(fileData, fileData2);
-//            System.out.println(p.compareLists());
-            p.compareLists();
             }
+            System.out.println("LINE1:" + fileData);
+            System.out.println("LINE2:" + fileData2);
+            AdventDay p = new AdventDay(fileData, fileData2);
+            System.out.println(p.compareLists());
             return fileData;
         }
         catch (FileNotFoundException e) {
